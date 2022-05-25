41f1d9b88fb84359eeb52ba101ce99f3f34f1774
Tavbulatov <alikhantam26@gmail.com>
2022-05-25 16:38:23 +0300

    active record default time zone

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module TestGuru
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Moscow"
+    config.active_record.default_timezone = 'Moscow'
+    config.time_zone = 'Moscow'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
