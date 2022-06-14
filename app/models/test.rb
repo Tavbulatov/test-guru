12a42f310cfcf549f93b1c6738addb399b2e065a
Tavbulatov <alikhantam26@gmail.com>
2022-06-15 00:23:59 +0300

    new method model test and model user

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.category_title(category)
+  category_id = Category.where(title: category).ids
+  Test.where(category_id: category_id).order(title: :desc).pluck(:title)
+  end
 end
