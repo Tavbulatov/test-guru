04ac62002a1f4a782e1eb633092e8dadc577c0e4
Tavbulatov <alikhantam26@gmail.com>
2022-06-19 02:04:21 +0300

    ref method names and fixing the query

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
-  def self.category_title(category)
-   joins('JOIN categories on tests.category_id = categories.id')
-  .where(categories: {title: category}).order(title: :desc).pluck('title')
+  def self.tests_by_category_title(category)
+    joins('JOIN categories on tests.category_id = categories.id')
+      .where(categories: {title: category})
+      .order(title: :desc)
+      .pluck(:tests.title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  def tests_by_level(level)
-    TestPassage.joins('JOIN tests on test_passages.test_id = tests.id')
-    .where(user_id: id, tests: { level: level}).pluck('title')
+  def tests_by_level_title(level)
+    Test.joins('JOIN test_passages on test_passages.test_id = tests.id')
+      .where(level: level, test_passages: { user_id: id })
   end
 end
