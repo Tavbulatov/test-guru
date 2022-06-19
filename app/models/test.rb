0e555cc71408d3f20c1ad4e818aa6c98e9fd87e5
Tavbulatov <alikhantam26@gmail.com>
2022-06-19 16:40:45 +0300

    rubocop check

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.tests_by_category_title(category)
     joins('JOIN categories on tests.category_id = categories.id')
-      .where(categories: {title: category})
+      .where(categories: { title: category })
       .order(title: :desc)
       .pluck(:title)
   end
