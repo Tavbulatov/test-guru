032a1428f60fde5d27dc4a49e92fd081a7131e6b
Tavbulatov <alikhantam26@gmail.com>
2022-07-01 20:41:32 +0300

    correct style model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
 
   def tests_by_level_title(level_name)
     tests.where(level: level_name)
